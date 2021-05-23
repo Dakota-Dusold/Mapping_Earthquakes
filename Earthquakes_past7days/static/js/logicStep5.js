4ab50d978e29ddbf101cd2533db8584623874266
Barbara Warnock <dakota.dusold@gmail.com>
2021-05-23 15:06:28 -0400

    added legend for magnitude colors

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -1,25 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the dark view tile layer that will be an option for our map.
+// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+	maxZoom: 18,
+	accessToken: API_KEY
 });
-
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Streets: streets,
-  Satellite: satelliteStreets
+  "Streets": streets,
+  "Satellite": satelliteStreets
 };
 
 // Create the earthquake layer for our map.
@@ -28,14 +27,14 @@ let earthquakes = new L.layerGroup();
 // We define an object that contains the overlays.
 // This overlay will be visible all the time.
 let overlays = {
-  Earthquakes: earthquakes
-};
+	Earthquakes: earthquakes
+  };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
+	center: [39.5, -98.5],
+	zoom: 3,
+	layers: [streets]
 });
 
 // Then we add a control to the map that will allow the user to change
@@ -50,8 +49,8 @@ L.geoJson(data, {
     // We turn each feature into a circleMarker on the map.
     pointToLayer: function(feature, latlng) {
         console.log(data);
-        return L.circleMarker(latlng);
-      },
+        	return L.circleMarker(latlng);
+    },
     // We set the style for each circleMarker using our styleInfo function.
   style: styleInfo,
     // We create a popup for each circleMarker to display the magnitude and
@@ -60,11 +59,37 @@ L.geoJson(data, {
     layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
   }
 }).addTo(earthquakes);
-
-  // Then we add the earthquake layer to our map.
-  earthquakes.addTo(map);
-
 });
+
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+
+    let div = L.DomUtil.create('div', 'info legend');
+		const magnitudes = [0, 1, 2, 3, 4, 5];
+		const colors = [
+		"#98ee00",
+		"#d4ee00",
+		"#eecc00",
+		"#ee9c00",
+		"#ea822c",
+		"#ea2c2c",
+	];
+	// Looping through our intervals to generate a label with a colored square for each interval.
+	for (var i = 0; i < magnitudes.length; i++) {
+		console.log(colors[i]);
+		div.innerHTML +=
+		  "<i style='background: " + colors[i] + "'></i> " +
+		  magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+	 }
+	return div;
+	// };
+};
+
+legend.addTo(map);
+
+//Then we add the earthquake layer to our map
+	earthquakes.addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into two separate functions
